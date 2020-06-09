d076276471eb4143b2e68fbe6748a4ff2928059b
Noel Kirkland <noelkirkland@gmail.com>
2020-06-09 16:41:38 -0700

    add several blank arrays
    co-authored-by: David Cole <davidtcole@yahoo.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,21 @@
-$document.ready(function(){
+$(document).ready(function(){
   $("form#fortune-questions").submit(function(event){
     event.preventDefault();
     $("#your-fortune").show();
+    let unluckyArray = [];
+    let luckyArray = [];
+    let allResponseArray = unluckyArray.concat(luckyArray);
+    
     $("input:checkbox[name=unlucky-events]:checked").each(function(){
-      const unluckyResponses = $(this).val();
+      unluckyArray.push($(this).val());
     });
     $("input:checkbox[name=lucky-events]:checked").each(function(){
-      const luckyResponses = $(this).val();
+      luckyArray.push($(this).val());
+      // oh cool! Did you just combine these two lines?
+      // Yeah, it should work to auto push each checked box into the array.
+      // Did you happen to see my concat let above? Do you think something like that would work?
+      // It's EXACTLY the way you want to do that. It'll work
+      // I am not sure if it's the best way, but I'm still feeling things out
     });
   });
 });
